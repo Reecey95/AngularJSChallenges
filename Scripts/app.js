e5c582f6457c791177687a551ce8a3844099459a
Reece Hall <reecehall_rc@blueyonder.co.uk>
2017-04-01 17:34:34 +0100

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,45 @@
+
+
+// var App = angular.module('App', []);
+//
+// App.controller('TodoCtrl', function($scope, $http) {
+//     $http.get('../todos.json')
+//         .then(function(res){
+//             $scope.todos = res.data;
+//         });
+// });
+//
+// App.controller('CatCtrl', function($scope, $http) {
+//     $http.get('../information.json')
+//         .then(function(res){
+//             $scope.information = res.data;
+//         });
+// });
+
+
+
+// 'use strict';
+//
+// // Define the `phonecatApp` module
+// var phonecatApp = angular.module('phonecatApp', []);
+//
+// // Define the `PhoneListController` controller on the `phonecatApp` module
+// phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
+//     $scope.phones = [
+//     {
+//         name: 'Nexus S',
+//         snippet: 'Fast just got faster with Nexus S.'
+//     }, {
+//         name: 'Motorola XOOM™ with Wi-Fi',
+//         snippet: 'The Next, Next Generation tablet.'
+//     }, {
+//         name: 'MOTOROLA XOOM™',
+//         snippet: 'The Next, Next Generation tablet.'
+//     }
+//     ];
+// });
+
+'use strict';
+
+// Define the `phonecatApp` module
+angular.module('phonecatApp', []);
